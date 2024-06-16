ef6da3a89da4feb68d09f758cbdfadb0e1b3eb3b
warchild <wolf@lycanthropy.us>
2024-06-16 14:04:23 -0700

    routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
 
@@ -10,11 +11,64 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Routes
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+app.get('/api/notes', (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(JSON.parse(data));
+    res.json(JSON.parse(data));
+  })
+});
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.post('/api/notes', (req, res) => {
+  // uuidv4();
+  console.log(req.body);
+  const notes = {
+    ...req.body, 
+    id: uuidv4(),
+  }
+  console.log(notes);
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const newNotes = JSON.parse(data);
+    newNotes.push(notes);
+    fs.writeFile("./db/db.json", JSON.stringify(newNotes), (err) => {
+      if (err) throw err;
+      res.json(newNotes);
+    })
+  })
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  console.log(req.params.id);
+  fs.readFile(
+    "./db/db.json",
+    "utf8",
+    (err, data) => {
+      if (err) throw err;
+      let newNotes = JSON.parse(data);
+      newNotes = newNotes.filter((note) => note.id!== req.params.id);
+      fs.writeFile("./db/db.json", JSON.stringify(newNotes), (err) => {
+        if (err) throw err;
+        res.json(newNotes);
+      })
+    }
+  )
+})
+
+//homepage 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+//notes 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
